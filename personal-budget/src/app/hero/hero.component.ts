3a54bc540f144c0275a0efee3aa39135a47c30a4
K <80170478+ekayembe@users.noreply.github.com>
2025-09-21 23:46:36 -0400

    add 2

--- personal-budget/src/app/hero/hero.component.ts
+++ personal-budget/src/app/hero/hero.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'pb-hero',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
